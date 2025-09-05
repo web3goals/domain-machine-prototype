5d617876d3baa60ce60e4e2d3ef5737d7457afc1
kiv1n <aloha@kiv1n.ru>
2025-09-05 10:58:47 +0300

    Add site configuration and update metadata in layout

---
+++ app/config/site.ts
@@ -0,0 +1,8 @@
+export const siteConfig = {
+  name: "Domain Machine",
+  description: "", // TODO: Fill in description
+  links: {
+    github: "https://github.com/web3goals/domain-machine-prototype",
+    twitter: "https://twitter.com/kiv1n",
+  },
+};
