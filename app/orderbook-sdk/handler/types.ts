6e77d9e526993f1e00ed99d7e182ca4af84c00ba
kiv1n <aloha@kiv1n.ru>
2025-09-09 08:04:31 +0300

    Delete unused orderbook SDK

--- app/orderbook-sdk/handler/types.ts
+++
@@ -1,3 +0,0 @@
-export interface BlockchainOperationHandler<TParams, TResult> {
-  execute(params: TParams): Promise<TResult>;
-}
--- app/orderbook-sdk/utils/date.utils.ts
+++
@@ -1,4 +0,0 @@
-export const SECONDS_IN_MINUTE = 60;
-export const MILLISECONDS_IN_SECOND = 1000;
-export const MINUTES_IN_HOUR = 60;
-export const HOURS_IN_DAY = 24;
