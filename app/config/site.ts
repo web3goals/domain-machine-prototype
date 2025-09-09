5db9adf146fc0f9f62104d6207b14bae0c401c1c
kiv1n <aloha@kiv1n.ru>
2025-09-09 08:29:20 +0300

    Update site description

--- app/config/site.ts
+++ app/config/site.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
   name: "Domain Machine",
-  description: "", // TODO: Fill in description
+  description: "Gamified Domain Trading with Mystery Boxes", // TODO: Fill in description
   links: {
     github: "https://github.com/web3goals/domain-machine-prototype",
     twitter: "https://twitter.com/kiv1n",
